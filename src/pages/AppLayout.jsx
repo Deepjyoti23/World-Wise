e454f1436bc9ffeaf97f04e627998706841a836f
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-08-25 19:39:29 +0530

    add user component

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,20 @@
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
-import styles from './AppLayout.module.css'
+import styles from "./AppLayout.module.css";
+import User from "../components/User";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppLayout() {
-  return <div className={styles.app}>
-    <Sidebar />
-    <Map />
-  </div>;
+  return (
+    <ProtectedRoute>
+      <div className={styles.app}>
+        <Sidebar />
+        <Map />
+        <User />
+      </div>
+      ;
+    </ProtectedRoute>
+  );
 }
 
 export default AppLayout;
