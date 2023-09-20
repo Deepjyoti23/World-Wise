65b55177fa248f83bfb95dab991f3c6ec4002741
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-09-21 00:14:10 +0530

    ..

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://json-servers.vercel.app";
 
 const CitiesContext = createContext();
 
